d7d9e9c06ddda6615006ab82d6e3c937bd34bf05
Taner105 <taneraydogan59@gmail.com>
2022-02-02 09:49:41 +0300

    .

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,31 +4,31 @@ import * as ReactBootStrap from "react-bootstrap";
 
 const Main = () => {
 
-    const [posts, setPosts] = useState({ blogs: [] })
+    const [posts, setPosts] = useState([])
     // console.log(posts);
     const [searchCapital, setSearchCapital] = useState("")
 
-    useEffect(() => {
-        const postList = async () => {
-            const { data } = await axios("https://restcountries.com/v2/all")
-            setPosts({ blogs: data })
-            console.log(data);
-        }
-        postList()
-    }, [setPosts])
-
     // useEffect(() => {
-    //     axios.get("https://restcountries.com/v2/all")
-    //         .then(res => {
-    //             setSearchCapital(res.data)
-    //             console.log(res.data);
-    //         })
-    //         .catch(error => console.log(error))
-    // }, []);
+    //     const postList = async () => {
+    //         const { data } = await axios("https://restcountries.com/v2/all")
+    //         setPosts({ blogs: data })
+    //         console.log(data);
+    //     }
+    //     postList()
+    // }, [setPosts])
 
-    const filteredCapital = () => {
-        posts.blogs.filter(post => post.capital.toLowerCase().includes(searchCapital.toLowerCase()))
-    }
+    useEffect(() => {
+        axios.get("https://restcountries.com/v2/all")
+            .then(res => {
+                setPosts(res.data)
+                console.log(res.data);
+            })
+            .catch(error => console.log(error))
+    }, []);
+
+    // const filteredCapital = () => {
+    //     posts.blogs.filter(post => post.capital.toLowerCase().includes(searchCapital.toLowerCase()))
+    // }
     return (
         <div>
             <ReactBootStrap.InputGroup className="mb-3" >
@@ -37,14 +37,13 @@ const Main = () => {
                     aria-label="Recipient's username"
                     aria-describedby="basic-addon2"
                     type="search"
-                    value={searchCapital}
+                    // value={searchCapital}
                     onChange={(e) => setSearchCapital(e.target.value)}
                 />
                 <ReactBootStrap.Button variant="outline-secondary" id="button-addon2">
                     Button
                 </ReactBootStrap.Button>
             </ReactBootStrap.InputGroup>
-
 
             <ReactBootStrap.Table striped bordered hover>
                 <thead>
@@ -58,13 +57,20 @@ const Main = () => {
                 </thead>
                 <tbody>
                     {
-                        filteredCapital.blogs && filteredCapital.blogs.map((item) => (
-                            <tr key={item.id}>
+                        posts.filter((item) => {
+                            if (searchCapital == "") {
+                                return item
+                            }
+                            else if (item[0][0].capital.toLowerCase().includes(searchCapital.toLowerCase())) {
+                                return item
+                            }
+                        }).map((item, index) => (
+                            <tr key={index}>
 
-                                <td>{item.blogs.name}</td>
-                                <td>{item.blogs.capital}</td>
-                                <td>{item.blogs.region}</td>
-                                <td><img style={{ width: "180px", height: "90px" }} src={item.blogs.flag} alt="flag" /></td>
+                                <td>{item.name}</td>
+                                <td>{item.capital}</td>
+                                <td>{item.region}</td>
+                                <td><img style={{ width: "180px", height: "90px" }} src={item.flag} alt="flag" /></td>
                             </tr>
 
                         ))
